40975950e567a0ee2a4e07fed3bb0759e3aace36
FahriDarmawan <44422245+FahriDarmawan@users.noreply.github.com>
2021-10-02 12:50:19 +0700

    Setup and Fire Analytics

---
+++ IncrementalGame(Firebase)/Assets/Scripts/TapArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+
+    public AudioSource CoinTapSfx;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        CoinTapSfx.Play();
+    }
+}
